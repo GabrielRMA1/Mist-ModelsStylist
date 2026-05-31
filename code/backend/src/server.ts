5bdf26ab2f00aaad92d4126e456399da94bcde86
Gabriel Rodrigues <gabriel.abreu@sga.pucminas.br>
2026-05-31 16:47:48 -0300

    docs: video sistema

--- code/backend/src/server.ts
+++ code/backend/src/server.ts
@@ -15,3 +15,33 @@ try {
   app.log.error(error);
   process.exit(1);
 }
+
+/*
+import "dotenv/config";
+
+import { app } from "./app.js";
+
+import { rabbitMQConfig } from "./config/rabbitmq/index.js";
+import { AgendamentoWorker } from "./workers/agendamentoWorker.js";
+
+const port = Number(process.env.PORT) || 3333;
+
+try {
+
+  await rabbitMQConfig.connect();
+
+  const agendamentoWorker = new AgendamentoWorker();
+  await agendamentoWorker.iniciar();
+
+  await app.listen({
+    port,
+    host: "0.0.0.0",
+  });
+
+  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Documentação disponível em http://localhost:${port}/docs`);
+} catch (error) {
+  app.log.error(error);
+  process.exit(1);
+}
+*/
